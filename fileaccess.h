0db22f1eb50e3cf0cf8f55a913048d0586c94915
Jan Marker <jan@jangmarker.de>
2017-12-30 23:29:55 +0100

    fix warning about trailing ;

--- fileaccess.h
+++ fileaccess.h
@@ -11,6 +11,6 @@ namespace FileAccess
     std::vector<Document> documentsFromDirectory(const std::experimental::filesystem::path& dir);
     Document documentFromFile(const std::experimental::filesystem::path& file);
 
-};
+}
 
 #endif //TT1_INFORMATION_RETRIEVAL_FILEACCESS_H
