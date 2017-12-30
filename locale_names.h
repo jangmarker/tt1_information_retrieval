5667f684e8d18640eb536b3343efbd13319bcdf4
Jan Marker <jan@jangmarker.de>
2017-12-30 23:27:35 +0100

    try to fix locale on windows

---
+++ locale_names.h
@@ -0,0 +1,10 @@
+#ifndef TT1_INFORMATION_RETRIEVAL_LOCALE_H
+#define TT1_INFORMATION_RETRIEVAL_LOCALE_H
+
+#ifdef _WIN32
+static const char* DE_LOCALE = "de-DE";
+#else
+static const char* DE_LOCALE = "de_DE.UTF-8";
+#endif
+
+#endif //TT1_INFORMATION_RETRIEVAL_LOCALE_H
--- test_helper.h
+++ test_helper.h
@@ -1,11 +1,13 @@
 #ifndef TT1_INFORMATION_RETRIEVAL_TEST_HELPER_H
 #define TT1_INFORMATION_RETRIEVAL_TEST_HELPER_H
 
+#include "locale_names.h"
+
 #include <locale>
 
 class LocaleSetter {
 public:
-    explicit LocaleSetter(std::string_view locale = "de_DE.UTF-8");
+    explicit LocaleSetter(std::string_view locale = DE_LOCALE);
     ~LocaleSetter();
 
 private:
